131a196973387d8f4565e2458a36a9c596ca7a14
panditamir <tntamir2023@gmail.com>
2024-08-05 21:46:42 +0545

    Under Construction first phase

--- blog/app/models.py
+++ blog/app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class UserProfile(models.Model):
+    pass
+
+class Project(models.Model):
+    pass
+
+class skill(models.Model):
+    pass
+
+class Contact(models.Model):
+    pass
